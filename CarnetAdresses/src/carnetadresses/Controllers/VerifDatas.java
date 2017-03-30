ce31fdf8a8b9c53d2a9834f985e8ddcf6be75767
Alex3132 <alexis.zueras.pro@gmail.com>
2017-03-30 10:21:34 +0200

    verif datas regexp en commentaire

--- CarnetAdresses/src/carnetadresses/Controllers/VerifDatas.java
+++ CarnetAdresses/src/carnetadresses/Controllers/VerifDatas.java
@@ -17,6 +17,7 @@ public class VerifDatas {
      * @param ret
      */
     private static void VerifyTel(boolean ret) {
+       // ^0[1-9][0-9]{8}$
         // TODO implement here
     }
 
@@ -24,6 +25,7 @@ public class VerifDatas {
      * @param ret
      */
     private static void VerifyCP(boolean ret) {
+        ///^(([0-8][0-9])|(9[0-5]))[0-9]{3}$/
         // TODO implement here
     }
 
@@ -32,6 +34,10 @@ public class VerifDatas {
      */
     private static void VerifData(boolean ret) {
         // TODO implement here
+    }
+    
+    private static void VerifMail(boolean ret){
+        //^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$
     }
 
 }
