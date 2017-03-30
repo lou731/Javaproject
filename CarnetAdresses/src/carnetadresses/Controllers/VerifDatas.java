1caf8c227d263eb551088c4c412139280e2f3dfc
Tfdi <tfdi@wanadoo.fr>
2017-03-30 13:14:37 +0200

    Modif controller verifdatas

--- CarnetAdresses/src/carnetadresses/Controllers/VerifDatas.java
+++ CarnetAdresses/src/carnetadresses/Controllers/VerifDatas.java
@@ -2,6 +2,7 @@ package carnetadresses.Controllers;
 
 import carnetadresses.Models.Contact;
 import java.util.*;
+import java.util.regex.*;
 
 /**
  * 
@@ -15,18 +16,65 @@ public class VerifDatas {
     {
         return true;
     }
-
+    
+    private static boolean controlStr(String chaine, int Longueur)
+        {
+             if(chaine.length()<Longueur+1)
+                 {
+                     return true;
+                 }
+                 else
+                 {
+                     return false;
+                 }
+        }
+    
     /**
      * @param ret
      */
-    private static boolean VerifyTel(String tel) {
-        return true;
+    private static boolean VerifyTel(String tel) 
+    {
+        Pattern pat=Pattern.compile("^(0|+33)[1-9]([-. ]?(0-9){2}){4}$");
+        Matcher regok=pat.matcher(tel);
+        if(regok.find())
+            {
+                 return true;
+            }
+            else
+            {
+                 return false;
+            }
     }
 
     /**
      * @param ret
      */
-    private static boolean VerifyCP(String cp) {
-        return true;
+    private static boolean VerifyCP(String cp) 
+    {
+        Pattern pat=Pattern.compile("^[0-9]{5}$");
+        Matcher regok=pat.matcher(cp);
+        if(regok.find())
+            {
+                 return true;
+            }
+            else
+            {
+                 return false;
+            }
     }
+    
+    private static boolean VeriofyEmail(String email)
+        {
+            Pattern pat=Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+            Matcher regok=pat.matcher(email);
+            if(regok.find())
+                {
+                     return true;
+                }
+                else
+                {
+                     return false;
+                }
+        }
+    
 }
--- Doc/SQL/projet.sql
+++ Doc/SQL/projet.sql
@@ -8,16 +8,16 @@
 
 CREATE TABLE Contacts(
         Id         int (11) Auto_increment  NOT NULL ,
-        Nom        Varchar (100) NOT NULL ,
-        Prenom     Varchar (100) ,
-        Rue        Varchar (200) ,
-        Complement Varchar (200) ,
+        Nom        Varchar (30) NOT NULL ,
+        Prenom     Varchar (30) ,
+        Rue        Varchar (50) ,
+        Complement Varchar (50) ,
         CP         Varchar (5) ,
-        Ville      Varchar (200) ,
+        Ville      Varchar (50) ,
         Tel1       Varchar (12) ,
         Tel2       Varchar (12) ,
-        Mail1      Varchar (200) ,
-        Mail2      Varchar (200) ,
+        Mail1      Varchar (50) ,
+        Mail2      Varchar (50) ,
         PRIMARY KEY (Id )
 )ENGINE=InnoDB;
 
